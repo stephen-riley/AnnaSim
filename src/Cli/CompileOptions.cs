60c67434fe1eafae379efd15f8f584bc5b6dbaaa
Stephen Riley <jstephen.riley@yahoo.com>
2024-09-06 20:05:49 -0700

    can now run/debug after compile

--- src/Cli/CompileOptions.cs
+++ src/Cli/CompileOptions.cs
@@ -4,13 +4,28 @@ using CommandLine.Text;
 namespace AnnaSim.Cli;
 
 [Verb("cc", HelpText = "Compile a C program to ANNA assembly")]
-public class CompileOptions
+public class CompileCliOptions
 {
     [Option('f', "filename", Default = "-", HelpText = "Input filename (.c); do not specify to read from STDIN")]
     public string Filename { get; set; } = "";
 
     [Option('o', "output", Default = "-", HelpText = "Output filename (.asm); do not specify to output to STDOUT")]
     public string Output { get; set; } = "";
+
+    [Option('d', "debug", HelpText = "Debug after compiling")]
+    public bool Debug { get; set; }
+
+    [Option('v', "vt100", HelpText = "Debug using the \"pretty\" debugger after compiling")]
+    public bool Vt100 { get; set; }
+
+    [Option('r', "run", HelpText = "Run the program after compiling")]
+    public bool Run { get; set; }
+
+    [Option('i', "input", HelpText = "Specifiy inputs for `in` instruction")]
+    public IEnumerable<string> Inputs { get; set; } = null!;
+
+    [Option('s', "screendump", HelpText = "Dump screen to STDOUT after execution")]
+    public bool DumpScreen { get; set; }
 
     private static readonly UnParserSettings settings = new()
     {
@@ -24,9 +39,12 @@ public class CompileOptions
     {
         get
         {
-            yield return new Example("Compile a .c file to a .asm file", settings, new CompileOptions { Filename = "example.c", Output = "example.asm" });
-            yield return new Example("Compile a .c file piped from STDIN to a .asm file", settings, new CompileOptions { Output = "example.asm" });
-            yield return new Example("Compile a .c file STDOUT", settings, new CompileOptions { Filename = "example.c" });
+            yield return new Example("Compile a .c file to a .asm file", settings, new CompileCliOptions { Filename = "example.c", Output = "example.asm" });
+            yield return new Example("Compile a .c file piped from STDIN to a .asm file", settings, new CompileCliOptions { Output = "example.asm" });
+            yield return new Example("Compile a .c file STDOUT", settings, new CompileCliOptions { Filename = "example.c" });
+
+            yield return new Example("Run a .c file after compiling with an input", settings, new CompileCliOptions { Filename = "example.c", Run = true, Inputs = ["5"] });
+            yield return new Example("Debug a .c file after compiling the \"pretty\" debugger", settings, new CompileCliOptions { Filename = "example.c", Vt100 = true, Inputs = ["5"] });
         }
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -12,56 +12,81 @@ var cliParser = new Parser(settings =>
 
 try
 {
-    cliParser.ParseArguments<RunCliOptions, DebugCliOptions, CompileOptions>(args)
-        .WithParsed<RunCliOptions>(opt =>
-        {
-            var runner = new Runner(opt.Filename, opt.Inputs.ToArray());
-            runner.Run(opt.DumpScreen);
-        })
-        .WithParsed<DebugCliOptions>(opt =>
-        {
-            BaseDebugger debugger = opt.Vt100
-                ? new Vt100ConsoleDebugger(opt.Filename, opt.Inputs.ToArray(), opt.DebugCommands.ToArray())
-                : new ConsoleDebugger(opt.Filename, opt.Inputs.ToArray(), opt.DebugCommands.ToArray());
-
-            debugger.Run(opt.DumpScreen);
-        })
-        .WithParsed<CompileOptions>(opt =>
-        {
-            string src;
-            if (opt.Filename != "-")
-            {
-                src = File.ReadAllText(opt.Filename);
-            }
-            else
-            {
-                var sb = new StringBuilder();
-                string? s;
-                while ((s = Console.ReadLine()) != null)
-                {
-                    sb.AppendLine(s);
-                }
-                src = sb.ToString();
-            }
-
-            if (Compiler.TryCompile(opt.Filename != "-" ? opt.Filename : "STDIN", src, out var asm))
-            {
-                if (opt.Output != "-")
-                {
-                    File.WriteAllText(opt.Output, asm);
-                }
-                else
-                {
-                    Console.WriteLine(asm);
-                }
-            }
-            else
-            {
-                Environment.Exit(-1);
-            }
-        });
+    cliParser.ParseArguments<RunCliOptions, DebugCliOptions, CompileCliOptions>(args)
+        .WithParsed<RunCliOptions>(HandleRun)
+        .WithParsed<DebugCliOptions>(HandleDebug)
+        .WithParsed<CompileCliOptions>(HandleCompile);
 }
 catch (Exception e)
 {
     Console.Error.WriteLine(e.Message);
+}
+
+void HandleRun(RunCliOptions opt)
+{
+    var runner = new Runner(opt.Filename, opt.Inputs.ToArray());
+    runner.Run(opt.DumpScreen);
+}
+void HandleDebug(DebugCliOptions opt)
+{
+    BaseDebugger debugger = opt.Vt100
+                   ? new Vt100ConsoleDebugger(opt.Filename, opt.Inputs.ToArray(), opt.DebugCommands.ToArray())
+                   : new ConsoleDebugger(opt.Filename, opt.Inputs.ToArray(), opt.DebugCommands.ToArray());
+
+    debugger.Run(opt.DumpScreen);
+}
+
+void HandleCompile(CompileCliOptions opt)
+{
+    string src;
+    if (opt.Filename != "-")
+    {
+        src = File.ReadAllText(opt.Filename);
+    }
+    else
+    {
+        var sb = new StringBuilder();
+        string? s;
+        while ((s = Console.ReadLine()) != null)
+        {
+            sb.AppendLine(s);
+        }
+        src = sb.ToString();
+    }
+
+    if (Compiler.TryCompile(opt.Filename != "-" ? opt.Filename : "STDIN", src, out var asm))
+    {
+        if (opt.Debug || opt.Vt100 || opt.Run)
+        {
+            var tmp = Path.GetTempFileName();
+            Console.Error.WriteLine($"writing assembly to temp file {tmp}");
+            File.WriteAllText(tmp, asm);
+
+            if (opt.Debug || opt.Vt100)
+            {
+                BaseDebugger debugger = opt.Vt100
+                   ? new Vt100ConsoleDebugger(tmp, opt.Inputs.ToArray(), [])
+                   : new ConsoleDebugger(tmp, opt.Inputs.ToArray(), []);
+
+                debugger.Run(opt.DumpScreen);
+            }
+            else
+            {
+                var runner = new Runner(tmp, opt.Inputs.ToArray());
+                runner.Run(opt.DumpScreen);
+            }
+        }
+        else if (opt.Output != "-")
+        {
+            File.WriteAllText(opt.Output, asm);
+        }
+        else
+        {
+            Console.WriteLine(asm);
+        }
+    }
+    else
+    {
+        Environment.Exit(-1);
+    }
 }
